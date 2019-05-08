8808c6f68e5db32c8cb176452d3561a9dde88a8b
chaoqunzhan <1129619959@qq.com>
2019-05-08 22:28:56 +0800

    uni

--- shareBook/pages/index/index.vue
+++ shareBook/pages/index/index.vue
@@ -16,7 +16,7 @@
 			</view>
 		</view>
 		
-		<scroll-view class="content" v-bind:style="{height:contentH+'px'}" scroll-y="true" @scrolltolower="loadMore" lower-threshold="100">
+		<scroll-view class="content" v-bind:style="{height:contentH+'px'}" scroll-y="true" @scrolltolower="loadMore" lower-threshold="10">
 			<view class="bonner">
 				<swiper class="swiper" :indicator-dots=true :autoplay=true :interval=5000 :duration=500>
 					<swiper-item>
@@ -65,7 +65,10 @@
 					</view>
 				</view>
 			</view>
+			<view class="noMore" v-if="showNoMore" >我也是有底线的！！！</view>
 		</scroll-view>
+		
+		<!-- <view class="noMore" v-if="showNoMore" >我也是有底线的！！！</view> -->
 	</view>
 </template>
 
@@ -132,20 +135,20 @@
 					cardTitle:"我是第十二张图片",
 					cardText:"来见识我的瀑布流啊来见识我的瀑布流啊来见识我的瀑布流啊来见识我的瀑布流啊"
 				}],
-				cardListLeft:[],
-				cardListRight:[],
+				cardListLeft:[],		//用来储存左栏的card
+				cardListRight:[],		//用来储存右栏的card
 				cardLeftHeight:0,
-				cardRightHeight:0,
-				cardListItem:0,
-				rImgH:0,
-				preLoadImg:"",
-				
-				contentH:1000
+				cardRightHeight:0,		//分别是左右栏的高度
+				cardListItem:0,			//作为card的ID
+				rImgH:0,				//实际载入的card的高度
+				contentH:800,			//设置客户端的屏幕高度默认值
+				loadMoreTemp:1,			//作为scoll滚动触底，加载更多的标记，防止多次出发事件，1为允许，0为阻止
+				showNoMore:false		//控制底部“我也是有底线view的显示”
 			}
 		},
 		
 		onLoad() {
-			this.waterfall();
+			this.waterfall();			//初始化瀑布流
 		},
 		methods: {
 			SearchClear(){
@@ -158,48 +161,39 @@
 				this.SearchData.value = event.target.value
 			},
 			
-			// preImageLoad:function(pre){
-			// 	let divWidth = 345;
-			// 	let oImgW = pre.detail.width; //图片原始宽度
-			// 	let oImgH = pre.detail.height; //图片原始高度
-			// 	this.rImgH = divWidth*oImgH/oImgW+212;
-			// },
 			
 			onImageLoad: function(e){
 
-				let divWidth = 345;
+				let divWidth = 345;			//实际显示的单栏宽度，345upx
 				let oImgW = e.detail.width; //图片原始宽度
 				let oImgH = e.detail.height; //图片原始高度
-				let rImgH = divWidth*oImgH/oImgW+170;
+				let rImgH = divWidth*oImgH/oImgW+170;	//重新计算当前载入的card的高度
+				
 				if(this.cardListItem==0){
-					this.cardLeftHeight += rImgH;	//第一张图高度加到cardLeftHeight
-					this.cardListItem++;			//图片索引加1
-					this.cardListRight.push(this.cardList[this.cardListItem]);	//添加第二张图到cardListRight数组
+					this.cardLeftHeight += rImgH;	//第一张card高度加到cardLeftHeight
+					this.cardListItem++;			//card索引加1
+					this.cardListRight.push(this.cardList[this.cardListItem]);	//添加第二张card到cardListRight数组
 				}else{
-					this.cardListItem++;		//图片索引加1
-						if(this.cardLeftHeight > this.cardRightHeight){
-							this.cardRightHeight += rImgH;		//第二张图高度加到cardRightHeight
+					this.cardListItem++;		//card索引加1
+					
+						if(this.cardLeftHeight > this.cardRightHeight){		//把card的高度加到目前高度更低的栏中
+							this.cardRightHeight += rImgH;		//第二张card高度加到cardRightHeight
 						}else{
 							this.cardLeftHeight += rImgH;
 						}
 						
-					if(this.cardListItem<this.cardList.length){	
+					if(this.cardListItem<this.cardList.length){				//根据目前的栏高，把下一张card，push到低的那栏
 						if(this.cardLeftHeight > this.cardRightHeight){
-							this.cardListRight.push(this.cardList[this.cardListItem]);		//添加第三张图到cardListRight数组
+							this.cardListRight.push(this.cardList[this.cardListItem]);		//添加第三张card到cardListRight数组
 						}else{
 							this.cardListLeft.push(this.cardList[this.cardListItem]);
 						}
 					}
 				}
 				
-				console.log(this.cardListItem);
-				if(this.cardListItem%4 == 0){
-					console.log("rightHeight:"+this.cardRightHeight);
-					console.log("leftHeight:"+this.cardLeftHeight);
-					var contentHupx;
-					this.cardLeftHeight > this.cardRightHeight ? contentHupx = this.cardLeftHeight : contentHupx = this.cardRightHeight;
-					this.contentH = uni.upx2px(contentHupx+40);
-					console.log(this.contentH);
+				// console.log(+this.cardListItem);
+				if(this.cardListItem%4 == 0){				//每次载入的card数量设置为4，只有载入完成才允许下一次的scroll触底，触发loadMore
+					this.loadMoreTemp = 1;
 				}
 				
 				
@@ -209,20 +203,36 @@
 				this.cardList = this.allcardList.slice(0,4);		//初始化图片显示
 				this.cardListLeft.push(this.cardList[0]);
 				this.preLoadImg = this.cardList[0].cardImg;
+				var that = this;
+				uni.getSystemInfo({		//利用uni-APP获取系统信息Api，获取客户端的屏幕高度，设置成scoll-view的高度，实现触底事件
+					success: function (res) {
+						that.contentH = res.windowHeight;
+					},
+				});
 			},
 			
 			loadMore: function(){
-				console.log("loadMore");
-				// console.log(this.cardList);
-				let newcardList = this.allcardList.slice(this.cardListItem,this.cardListItem+4);
-				// console.log(newcardList);
-				this.cardList = this.cardList.concat(newcardList);
-				console.log(this.cardList);
-				if(this.cardLeftHeight > this.cardRightHeight){
-					this.cardListRight.push(newcardList[0]);
-				}else{
-					this.cardListLeft.push(newcardList[0]);
+				if(this.loadMoreTemp == 1){			//loadMoreTemp==1,才允许触发
+					console.log("loadMore");
+					this.loadMoreTemp = 0;			//防止多次触发
+					
+					let newcardList = this.allcardList.slice(this.cardListItem,this.cardListItem+4);//模拟后端接口返回四个新的数据
+					
+					//console.log(newcardList);
+					if(!newcardList.length == 0){				//判断是否还有新数据
+						this.cardList = this.cardList.concat(newcardList);			//返回的新数据加到当前的cardList
+						if(this.cardLeftHeight > this.cardRightHeight){				//把第一个新数据加到目前更低的栏上，以触发@load="onImageLoad"
+							this.cardListRight.push(newcardList[0]);			
+						}else{
+							this.cardListLeft.push(newcardList[0]);
+						}
+					}else{
+						this.showNoMore = true;				//没有新数据就显示到底了
+					}
+					
+					
 				}
+				
 			}
 		}
 	}
@@ -418,6 +428,13 @@
 	display: -webkit-box;
 	-webkit-line-clamp: 2;
 	-webkit-box-orient: vertical;
+}
+
+.noMore{
+	width:100%;
+	background:#f0f0f0;
+	font-size:24upx;
+	color:#808080;
 }
 
 </style>
